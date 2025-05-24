ecd8829e06fdc9bcff9e66e7835518458cdf3f58
KousikTheHunk <kousik.chandra@yahoo.com>
2025-05-24 07:23:23 +0530

    projectData.js update 2

--- src/utlits/fackData/projectsData.js
+++ src/utlits/fackData/projectsData.js
@@ -1,4 +1,4 @@
-import work_1 from "../../assets/images/projects/work1.jpeg"
+import work_1 from "../../assets/images/projects/work1.png"
 import work_2 from "../../assets/images/projects/work2.jpeg"
 import work_3 from "../../assets/images/projects/work3.jpeg"
 import work_4 from "../../assets/images/projects/work4.jpeg"
